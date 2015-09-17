ae177a7c522d62623fbf40fb8b07a85e7ac1a71c
ravi580 <ravitej.nerella@gmail.com>
2015-09-18 00:54:28 +0530

    for reporting

--- eTemple.UI.Donations/DonorMasterReportUI.cs
+++ eTemple.UI.Donations/DonorMasterReportUI.cs
@@ -7,14 +7,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using eTemple.Data.Repositories;
+using eTemple.Data.Models;
 namespace eTemple.UI.Donations
 {
     public partial class DonorMasterReportUI : Form
     {
+        public ServiceTypeRepository oServiceTypeRep = null;
+        public ServiceNameRepository oServiceNameRep = null;
+        public List<ServiceType> lstServiceType=null;
         public DonorMasterReportUI()
         {
             InitializeComponent();
+            oServiceTypeRep = new ServiceTypeRepository();
+            oServiceNameRep = new ServiceNameRepository();
+            lstServiceType = oServiceTypeRep.GetAllAsQuerable().ToList();
+
         }
 
         private void DonorMasterReportUI_Load(object sender, EventArgs e)
@@ -34,7 +42,11 @@ namespace eTemple.UI.Donations
 
         private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            //DonorReportForm donorreportForm = new DonorReportForm();
+            //donorreportForm.Show();
+            //DonorReportDataSet ds = new DonorReportDataSet();
+            //DataTable dt = new DataTable();
+            //ds.DataTable1 = (DonorReportDataSet.DataTable1DataTable)dt;
         }
 
         private void radioButton1_CheckedChanged(object sender, EventArgs e)
@@ -46,18 +58,32 @@ namespace eTemple.UI.Donations
         {
             if (radioButton2.Checked)
             {
-                lblServiceName.Visible = true;
-                cmbServiceName.Visible = true;
-                
+                lblServiceType.Visible = true;
+                cmbServiceType.Visible = true;
+                cmbServiceType.DataSource = lstServiceType;
+                cmbServiceType.DisplayMember = "Name";
+                cmbServiceType.SelectedIndex = -1;
             }
             else
             {
+                lblServiceType.Visible = false;
+                cmbServiceType.Visible = false;
+                lblServiceName.Visible = false;
+                cmbServiceName.Visible = false;
             }
         }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
+            lblServiceName.Visible = true;
+            cmbServiceName.Visible = true;
+            var serviceType = cmbServiceType.SelectedItem as ServiceType;
+            if(serviceType != null)
+            {
+                cmbServiceName.DataSource = oServiceNameRep.GetAllAsQuerable().Where(sType => sType.ServiceTypeId == serviceType.Id).ToList();
+                cmbServiceName.DisplayMember = "Name";
+            }
+            cmbServiceName.SelectedIndex = -1;
         }
     }
 }
