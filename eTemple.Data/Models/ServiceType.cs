6cf9901defafd327d9e48fbfaea935bc89ab1265
ravi580 <ravitej.nerella@gmail.com>
2015-09-18 00:56:58 +0530

    for reporting

--- eTemple.Data/Models/ServiceType.cs
+++ eTemple.Data/Models/ServiceType.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using eTemple.Data.Repositories;
 
 namespace eTemple.Data.Models
 {
-    public partial class ServiceType
+    public partial class ServiceType : IEntity
     {
         [ResultColumn]
         public int Id { get; set; }
