e43d42cf24faa95b4c77f184b8cfd248c4ad4d4c
ravi580 <ravitej.nerella@gmail.com>
2015-09-18 00:57:11 +0530

    for reporting

--- eTemple.Data/Models/ServiceName.cs
+++ eTemple.Data/Models/ServiceName.cs
@@ -4,10 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using eTemple.Data.Repositories;
 
 namespace eTemple.Data.Models
 {
-    public partial class ServiceName
+    public partial class ServiceName : IEntity
     {
         [ResultColumn]
         public int Id { get; set; }
