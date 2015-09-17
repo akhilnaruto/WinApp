dabe389322fbf8c760ae941161ae592c8c1789dc
ravi580 <ravitej.nerella@gmail.com>
2015-09-17 20:42:09 +0530

    crystal reporting

---
+++ eTemple.UI.Donations/DonorReportForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace eTemple.UI.Donations
+{
+    public partial class DonorReportForm : Form
+    {
+        public DonorReportForm()
+        {
+            InitializeComponent();
+        }
+
+        private void DonorReportForm_Load(object sender, EventArgs e)
+        {
+
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
