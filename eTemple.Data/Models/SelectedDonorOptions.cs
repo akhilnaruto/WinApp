11775aa986c12ae6a65ee3e283bc76d345fb534a
prabhuashwin <ashwin.nprabhu@gmail.com>
2015-09-20 22:29:16 +0530

    For Saving Modal Window Data

---
+++ eTemple.Data/Models/SelectedDonorOptions.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTemple.Data.Models
+{
+    public class SelectedDonorOptions
+    {
+        public int Id { get; set; }
+
+
+        public DateTime date { get; set; }
+
+
+        public string Address { get; set; }
+
+
+        public string Surname { get; set; }
+
+
+        public string Name { get; set; }
+
+
+        public string DistrictName { get; set; }
+
+
+        public string City { get; set; }
+
+
+        public int Pin { get; set; }
+
+
+        public string State { get; set; }
+
+
+        public string Country { get; set; }
+
+
+        public string NameOn { get; set; }
+
+
+        public int Star { get; set; }
+
+
+        public string Occassion { get; set; }
+
+
+        public string Gothram { get; set; }
+
+
+        public int Amount { get; set; }
+
+
+        public int MR_No { get; set; }
+
+
+        public string Remarks { get; set; }
+
+
+        public string PhoneNumber { get; set; }
+
+
+        public int SpecialDayId { get; set; }
+
+
+        public int ServiceTypeId { get; set; }
+
+
+        public int ServiceNameId { get; set; }
+
+
+        public int DateTypeId { get; set; }
+
+
+        public DateTime PerformDate { get; set; }
+
+
+        public string EmailId { get; set; }
+
+
+        public string Paksha { get; set; }
+
+
+        public int Month { get; set; }
+
+
+        public int Thidhi { get; set; }
+    }
+}
