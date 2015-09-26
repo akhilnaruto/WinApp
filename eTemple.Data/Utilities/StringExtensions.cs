a7d46ebecb8260a9e8c91a6978bada5fdddc4545
ravi580 <ravitej.nerella@gmail.com>
2015-09-26 18:30:21 +0530

    for reporting

---
+++ eTemple.Data/Utilities/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTemple.Data.Utilities
+{
+    public static class StringExtensions
+    {
+        public static string NullString(this string s)
+        {
+            return string.IsNullOrEmpty(s) ? null : s;
+        }
+    }
+}
