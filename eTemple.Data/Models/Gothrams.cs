2ebe0faaadbc743b53c91afd8602948f7a8ec5cc
ravi580 <ravitej.nerella@gmail.com>
2015-09-27 11:13:03 +0530

    adding gothrams class

---
+++ eTemple.Data/Models/Gothrams.cs
@@ -0,0 +1,17 @@
+﻿using eTemple.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTemple.Data.Models
+{
+    public partial class Gothrams : IEntity
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
+
