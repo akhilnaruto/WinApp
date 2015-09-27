2700fa98e8031efd33e48cfcc196bb9b322b9fbe
prabhuashwin <ashwin.nprabhu@gmail.com>
2015-09-27 08:52:57 +0530

    Added form for Daily Annadanam and make some changes for Insert and update based on testing.

---
+++ eTemple.Data/Models/DailyAnnaDanamModel.cs
@@ -0,0 +1,22 @@
+﻿using eTemple.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eTemple.Data.Models
+{
+    public partial class DailyAnnaDanamModel : IEntity
+    {        
+        public int Id { get; set; }
+        
+        public string Name { get; set; }
+        
+        public string PhoneNumber { get; set; }
+        
+        public string Gothram { get; set; }
+        
+        public string VillageName { get; set; }
+    }
+}
