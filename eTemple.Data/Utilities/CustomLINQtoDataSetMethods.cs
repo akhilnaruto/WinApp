7b65bfee6cd798b5915583170ff70f969905fd06
ravi580 <ravitej.nerella@gmail.com>
2015-09-27 21:09:43 +0530

    for reporting

---
+++ eTemple.Data/Utilities/CustomLINQtoDataSetMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InternalTools
+{
+    public static class CustomLINQtoDataSetMethods
+    {
+        public static DataTable CopyobjToDataTable<T>(this IEnumerable<T> source)
+        {
+            return new ObjectShredder<T>().Shred(source, null, null);
+        }
+
+        public static DataTable CopyToDataTable<T>(this IEnumerable<T> source,
+                                                    DataTable table, LoadOption? options)
+        {
+            return new ObjectShredder<T>().Shred(source, table, options);
+        }
+
+    }
+}
